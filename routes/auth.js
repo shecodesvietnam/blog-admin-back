2a5bf676aaf56ad003c00d9fbf0d6653fd6e0d3e
Vu Huy <vuhuycto@gmail.com>
2021-05-27 23:51:01 +0700

    add validate admin

--- routes/auth.js
+++ routes/auth.js
@@ -15,8 +15,10 @@ router.post("/", async function handle(req, res) {
   const { error } = validate(req.body);
   if (error) return res.status(404).send(error.details[0].message);
 
-  let user = await User.findOne({ email: req.body.email });
+  const user = await User.findOne({ email: req.body.email });
   if (!user) return res.status(400).send("Invalid email");
+
+  if (!user.isAdmin) return res.status(400).send("Invalid admin");
 
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send("Invalid password");
