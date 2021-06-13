cfc6afbf8080557c31db84cccab7f116c908e190
Vu Huy <vuhuycto@gmail.com>
2021-06-13 20:03:49 +0700

    update db string

--- startup/db.js
+++ startup/db.js
@@ -5,13 +5,10 @@ require("dotenv").config();
 
 module.exports = function initDB() {
   mongoose
-    .connect(
-      "mongodb+srv://dhoang:123@cluster0.g1ooo.mongodb.net/test?retryWrites=true&w=majority",
-      {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-      }
-    )
+    .connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    })
     .then(function log() {
       console.log(`Connected to ${db}...`);
       Fawn.init(mongoose);
