797f9f3477f3df19d7cbf6064ac6d8cc9a49f5bd
Vu Huy <vuhuycto@gmail.com>
2021-06-13 20:34:16 +0700

    update validate create admin

--- models/users.js
+++ models/users.js
@@ -7,6 +7,7 @@ function validateUser(user) {
     name: Joi.string().min(5).max(50).required(),
     email: Joi.string().min(5).max(255).required().email(),
     password: Joi.string().min(5).max(1024).required(),
+    isAdmin: Joi.boolean(),
   });
 }
 
